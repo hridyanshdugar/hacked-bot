06342817f0515cd7a435750aa7c7949b5a14d5e6
Hridyansh Dugar <dugar@ualberta.ca>
2025-02-16 16:10:53 -0700

    wip

--- bot/Bot.py
+++ bot/Bot.py
@@ -374,7 +374,7 @@ class Bot(commands.Bot):
             for team_name, devpost, github in teams:
                 role = discord.utils.get(ctx.guild.roles, name=team_name)
                 members_with_role = role.members
-                member_lines = [member.mention for member in members_with_role]
+                member_lines = " ".join([member.mention for member in members_with_role])
                 lines.append(
                     f"**Team Name:** {team_name}\n"
                     f"• Devpost: {devpost}\n"
