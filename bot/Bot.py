8273680466c64ef476e9d1b56f6ae145da26d831
Hridyansh Dugar <dugar@ualberta.ca>
2025-02-16 15:14:04 -0700

    wip

--- bot/Bot.py
+++ bot/Bot.py
@@ -319,7 +319,7 @@ class Bot(commands.Bot):
             ))
             self.conn.commit()
 
-            await interaction.response.send_message(f"Please wait for the announcement before you run this command.")
+            # await interaction.response.send_message(f"Please wait for the announcement before you run this command.")
 
             await interaction.response.send_message(
                 ":white_check_mark: Your submission has been recorded!",
