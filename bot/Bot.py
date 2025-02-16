794b3a1ff0c68c840a77fb8466c203668b17b258
Hridyansh Dugar <dugar@ualberta.ca>
2025-02-16 13:08:39 -0700

    wip

--- bot/Bot.py
+++ bot/Bot.py
@@ -297,6 +297,6 @@ class Bot(commands.Bot):
             if interaction.channel.category.name.lower() == "general":
                 await interaction.response.send_message(f"❌ You cannot run this command here. Please run it in your team channel.")
                 return
-            await interaction.response.send_message(f":ok:")
+            await interaction.response.send_message(f"Please wait for the announcement before you can run this command.")
             return
 
